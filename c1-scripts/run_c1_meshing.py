4886e12189e2831e59d676401bad3b8be80ea379
Daniel Zint <daniel.zint@nyu.edu>
2025-01-12 08:51:20 -0500

    Get input generation and c1 run script to work.

--- c1-scripts/run_c1_meshing.py
+++ c1-scripts/run_c1_meshing.py
@@ -2,6 +2,7 @@ import json
 import sys
 import os
 import subprocess
+import traceback
 
 
 run_script_path = os.path.dirname(__file__)
@@ -15,39 +16,49 @@ def run_command(command):
         print(f"!!!!! Command '{command}' failed with error: {e} !!!!!")
 
 
-# read executables json
-with open(executables_json, "r") as f:
-    executables = json.load(f)
-
-seamless_parametrization_binary = executables["seamless_parametrization_binary"]
-polyfem_binary = executables["polyfem_binary"]
-smooth_contours_binary = executables["smooth_contours_binary"]
-c1_meshing_script = executables["c1_meshing_script"]
-
 if __name__ == "__main__":
     args = sys.argv
 
-    if len(args) < 2:
+    if len(args) < 1:
         print("Not enough arguments")
         exit()
 
-    # print("Executables:")
-    # for x in executables:
-    #     print("*", x, ":", executables[x])
-
     input_file = args[1]
-    output_name = args[2]
+
+    input_file_stem = os.path.splitext(input_file)[0]
+    input_file_basename = os.path.basename(input_file)
+    input_file_basename_stem = os.path.splitext(input_file_basename)[0]
+
+    input_file = os.path.abspath(input_file)
+
+    if len(args) > 2:
+        output_name = args[2]
+    else:
+        output_name = input_file_basename_stem
 
     if not os.path.isfile(input_file):
         print("Input file", input_file, " does not exist")
         exit()
 
-    # compute output_name from input_file
-    # create folder within working directory
-    # cd to that folder and run from there
+    ###########################
+    # read executables json
+    with open(executables_json, "r") as f:
+        executables = json.load(f)
 
+    seamless_parametrization_binary = executables["seamless_parametrization_binary"]
+    polyfem_binary = executables["polyfem_binary"]
+    smooth_contours_binary = executables["smooth_contours_binary"]
+    c1_meshing_script = executables["c1_meshing_script"]
+
+    # create folder within working directory
+    os.makedirs(output_name, exist_ok=True)
+
+    # cd to that folder and run from there
     command = " ".join(
         [
+            "cd",
+            output_name,
+            "; ",
             "python3",
             c1_meshing_script,
             input_file,
