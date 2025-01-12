9aaa93fa63b74d1a2e29be958545ddc8717d008b
JcDai <jiachengdai97@gmail.com>
2025-01-12 14:45:33 -0500

    only keep two layers

--- src/clough_tocher_patches/c1_meshing_pipeline.py
+++ src/clough_tocher_patches/c1_meshing_pipeline.py
@@ -76,31 +76,69 @@ if __name__ == "__main__":
     workspace_path = "./"
 
     tm = mio.read(input_file)
-    vertices = tm.points
-    tets = tm.cells_dict['tetra']
-    filtered_tets = []
-    winding_numbers_data = tm.cell_data['winding_number'][0]
-    # print(type(winding_numbers))
-    for i in range(tets.shape[0]):
-        if abs(winding_numbers_data[i]) >= 0.5:
-            filtered_tets.append(tets[i])
+    vertices_unsliced = tm.points
+    tets_unsliced = tm.cells_dict['tetra']
 
+    # check orientation, TODO: only do in debug
+    print("[{}] ".format(datetime.datetime.now()), "checking tet orientation ...")
+    for i in range(tets_unsliced.shape[0]):
+        if not orient3d(vertices_unsliced[tets_unsliced[i][0]], vertices_unsliced[tets_unsliced[i][1]], vertices_unsliced[tets_unsliced[i][2]], vertices_unsliced[tets_unsliced[i][3]]):
+            print("tet {} flipped".format(i))
+        assert orient3d(vertices_unsliced[tets_unsliced[i][0]], vertices_unsliced[tets_unsliced[i][1]], vertices_unsliced[tets_unsliced[i][2]], vertices_unsliced[tets_unsliced[i][3]])
+    print("[{}] ".format(datetime.datetime.now()), "passed orientation check.")
+
+    winding_numbers_data_unsliced = tm.cell_data['winding_number'][0]
+    
+    # remove tet not touching surface
+    filtered_tets = []
+    for i in range(tets_unsliced.shape[0]):
+        if abs(winding_numbers_data_unsliced[i]) >= 0.5:
+            filtered_tets.append(tets_unsliced[i])
+    filtered_tets = np.array(filtered_tets)
+
+    surface_tet_faces = igl.boundary_facets(filtered_tets)
+    surface_vertices = np.unique(surface_tet_faces)
+
+    tet_indices_touching_surface = np.unique(np.argwhere(np.isin(tets_unsliced, surface_vertices))[:,0])
+
+    tets = tets_unsliced[tet_indices_touching_surface]
+    winding_numbers_data = winding_numbers_data_unsliced[tet_indices_touching_surface]
+    vertices = vertices_unsliced
+    vertices, tets, _, sliced_to_unsliced_v_map = igl.remove_unreferenced(vertices_unsliced, tets)
+
+    # print(winding_numbers_data.shape)
+    # print(tets.shape)
+    m_sliced = mio.Mesh(vertices, [('tetra', tets)], cell_data={"winding_number": winding_numbers_data.T})
+    m_sliced.write("test_slice.vtu")
+
+    # extract surface
     winding_numbers = {}
     for i in range(tets.shape[0]):
         winding_numbers[i] = winding_numbers_data[i]
 
-    filtered_tets = np.array(filtered_tets)
+    # filtered_tets = np.array(filtered_tets)
+    # filtered_tets = []
+    # for i in range(tets.shape[0]):
+    #     if abs(winding_numbers_data[i]) >= 0.5:
+    #         filtered_tets.append(tets[i])
+    # filtered_tets = np.array(filtered_tets)
 
-    # check orientation, TODO: only do in debug
-    print("[{}] ".format(datetime.datetime.now()), "checking tet orientation ...")
-    for i in range(tets.shape[0]):
-        if not orient3d(vertices[tets[i][0]], vertices[tets[i][1]], vertices[tets[i][2]], vertices[tets[i][3]]):
-            print("tet {} flipped".format(i))
-        assert orient3d(vertices[tets[i][0]], vertices[tets[i][1]], vertices[tets[i][2]], vertices[tets[i][3]])
-    print("[{}] ".format(datetime.datetime.now()), "passed orientation check.")
+    surface_tet_faces_unsliced = igl.boundary_facets(filtered_tets)
+    unsliced_to_sliced_v_map = {}
+    for i in range(len(sliced_to_unsliced_v_map)):
+        unsliced_to_sliced_v_map[sliced_to_unsliced_v_map[i]] = i
+    surface_tet_faces = surface_tet_faces_unsliced.copy().tolist()
+    for i in range(len(surface_tet_faces)):
+        for j in range(3):
+            # print(surface_tet_face[i][j])
+            surface_tet_faces[i][j] = unsliced_to_sliced_v_map[surface_tet_faces[i][j]]
+    surface_tet_faces = np.array(surface_tet_faces)
+    # print(surface_tet_faces)
+
 
     # get surface mesh for parametrization
-    surface_tet_faces = igl.boundary_facets(filtered_tets)
+    # surface_tet_faces_filtered = igl.boundary_facets(filtered_tets)
+
     para_in_v, para_in_f, im, para_in_v_to_tet_v_map = igl.remove_unreferenced(vertices, surface_tet_faces)
     assert((igl.bfs_orient(para_in_f)[0] == para_in_f).all())
 
@@ -223,7 +261,9 @@ if __name__ == "__main__":
     # para_command = path_to_para_exe + " --mesh " + workspace_path + "embedded_surface.obj --fit_field --output " + workspace_path
     para_command = path_to_para_exe + " --mesh " + workspace_path + "embedded_surface.obj --fit_field"
     # print(para_command.split())
-    subprocess.run(para_command,  shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # subprocess.run(para_command,  shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(para_command,  shell=True, check=True)
+
     # subprocess.run(para_command.split(' '), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     # print("here")
 
