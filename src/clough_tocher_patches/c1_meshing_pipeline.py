5d8525abece86952a6c5a4cdf4e385d2fd11f0b2
JcDai <jiachengdai97@gmail.com>
2025-01-21 21:02:41 -0500

    add lap

--- src/clough_tocher_patches/c1_meshing_pipeline.py
+++ src/clough_tocher_patches/c1_meshing_pipeline.py
@@ -704,8 +704,8 @@ if __name__ == "__main__":
                         tet_surface[tid] = [f]
                         break
 
-    tets_regular = np.array(tets_regular)
-    tets_vertices_regular = np.array(tets_vertices_regular)
+    # tets_regular = np.array(tets_regular)
+    # tets_vertices_regular = np.array(tets_vertices_regular)
 
     print(
         "[{}] ".format(datetime.datetime.now()),
@@ -726,18 +726,112 @@ if __name__ == "__main__":
 
     subprocess.run(para_command, shell=True, check=True)
 
+    # rearrange cones
+    angles = np.loadtxt("embedded_surface_Th_hat")
+
+    cone_vids = []
+    for i, angle in enumerate(angles):
+        if angle < 6.0 or angle > 6.5:  # less or more than 2 * pi
+            cone_vids.append(i)
+    print("cone cnt: ", len(cone_vids))
+    v_embd, _, _, f_embd, _, _ = igl.read_obj("embedded_surface.obj")
+
+    adjlist = igl.adjacency_list(f_embd)
+    colored = [False] * v_embd.shape[0]
+    cone_list = copy.deepcopy(cone_vids)
+
+    cannot_place_id = []
+    placed_id = []
+    for c in cone_list:
+        if not colored[c]:
+            placed_id.append(c)
+            colored[c] = True
+            for adjv in adjlist[c]:
+                colored[adjv] = True
+        else:
+            cannot_place_id.append(c)
+
+    exchange_map = {}
+    second_failed = []
+
+    for c in cannot_place_id:
+        # try place in one ring
+        found = False
+        for adjv in adjlist[c]:
+            if not colored[adjv]:
+                exchange_map[c] = adjv
+                exchange_map[adjv] = c
+                colored[adjv] = True
+                for vv in adjlist[adjv]:
+                    colored[vv] = True
+                found = True
+                break
+        if not found:
+            second_failed.append(c)
+
+    third_failed = []
+    for c in second_failed:
+        found = False
+        for vv in range(v_embd.shape[0]):
+            if not colored[vv]:
+                exchange_map[c] = vv
+                exchange_map[vv] = c
+                colored[vv] = True
+                for vvv in adjlist[vv]:
+                    colored[vvv] = True
+                found = True
+                break
+        if not found:
+            third_failed.append(c)
+
+    if len(third_failed) != 0:
+        print("cannot easily place cone! try make mesh denser")
+        exit()
+
+    reorder_vid = []
+    for i in range(v_embd.shape[0]):
+        if i not in exchange_map:
+            reorder_vid.append(i)
+        else:
+            reorder_vid.append(exchange_map[i])
+
+    assert len(reorder_vid) == v_embd.shape[0]
+
+    with open("embedded_surface_Th_hat_reordered", "w") as file:
+        for i in range(v_embd.shape[0]):
+            file.write("{}\n".format(angles[reorder_vid[i]]))
+
+    angles_new = np.loadtxt("embedded_surface_Th_hat_reordered")
+    cone_vids_new = []
+    for i, angle_new in enumerate(angles_new):
+        if angle_new < 6.0 or angle_new > 6.5:  # less or more than 2 * pi
+            cone_vids_new.append(i)
+
+    assert len(cone_vids_new) == len(cone_vids)
+
+
     ####################################################
     #             Call Parametrization Code            #
     ####################################################
     print("[{}] ".format(datetime.datetime.now()), "Calling parametrization code")
-    # para_command = path_to_para_exe + " --mesh " + workspace_path + "embedded_surface.obj --fit_field"
+    # para_command = (
+    #     path_to_para_exe
+    #     + " --mesh "
+    #     + workspace_path
+    #     + "embedded_surface.obj --cones embedded_surface_Th_hat --field embedded_surface_kappa_hat"
+    # )
+    # para_command = (
+    #     path_to_para_exe
+    #     + " --mesh "
+    #     + workspace_path
+    #     + "embedded_surface.obj --cones embedded_surface_Th_hat_reordered --field embedded_surface_kappa_hat"
+    # )
     para_command = (
         path_to_para_exe
         + " --mesh "
         + workspace_path
-        + "embedded_surface.obj --cones embedded_surface_Th_hat --field embedded_surface_kappa_hat"
+        + "embedded_surface.obj --cones cone_angles.txt --field embedded_surface_kappa_hat"
     )
-    # para_command = path_to_para_exe + " --mesh " + workspace_path + "tet.obj --cones tet_Th_hat --field tet_kappa_hat"
 
     subprocess.run(
         para_command,
@@ -746,6 +840,17 @@ if __name__ == "__main__":
         stderr=subprocess.STDOUT,
         stdout=subprocess.DEVNULL,
     )
+
+    ####################################################
+    #       Split the tet with new para vertices       #
+    ####################################################
+
+
+
+
+    # do not comment out this !!!!
+    tets_regular = np.array(tets_regular)
+    tets_vertices_regular = np.array(tets_vertices_regular)
 
     ####################################################
     #             Call c1_meshing_split_app            #
@@ -757,6 +862,7 @@ if __name__ == "__main__":
     p_v, p_tc, _, p_f, p_ftc, _ = igl.read_obj(para_out_file)
     if p_v.shape[0] > para_in_v.shape[0]:
         print("do not support parametrization new vertex")
+        exit()
 
     with open("toolkit_map.txt", "w") as file:
         # {{1, 2, 3}, {0, 2, 3}, {0, 1, 3}, {0, 1, 2}}
@@ -780,7 +886,8 @@ if __name__ == "__main__":
                     found = True
             assert found
 
-    angles = np.loadtxt("embedded_surface_Th_hat")
+    # angles = np.loadtxt("embedded_surface_Th_hat")
+    angles = np.loadtxt("embedded_surface_Th_hat_reordered")
 
     cone_vids = []
     for i, angle in enumerate(angles):
@@ -1526,7 +1633,9 @@ if __name__ == "__main__":
     Beq = np.zeros((0,3))
 
     print("solving bilaplacian on upsampled")
-    v_smoothed = igl.min_quad_with_fixed(A, B, known, Y, Aeq, Beq, True)
+    # v_smoothed = igl.min_quad_with_fixed(A, B, known, Y, Aeq, Beq, True)
+    v_smoothed = scipy.sparse.linalg.spsolve(scipy.sparse.identity(L_w.shape[0]) - 0.1 * M_inv @ L_w, v_ct)
+
     igl.write_obj("sampled_after_smoothing.obj", v_smoothed[1], f_ct)
 
     m_xx_points = ryan_mesh.points.copy()
