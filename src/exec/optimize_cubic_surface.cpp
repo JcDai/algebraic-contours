fb9204638e009037335191faf7ef5db145ed7c60
Ryan Capouellez <rjc8237@nyu.edu>
2025-05-30 21:11:22 -0400

    Added refinement option

--- src/exec/optimize_cubic_surface.cpp
+++ src/exec/optimize_cubic_surface.cpp
@@ -3,6 +3,7 @@
 #include <CLI/CLI.hpp>
 #include <igl/readOBJ.h>
 #include <igl/writeOBJ.h>
+#include <igl/upsample.h>
 
 /**
  * @brief Executable to take a triangle mesh file (stored as OBJ) and produce an
@@ -10,16 +11,29 @@
  *
  */
 
-int main(int argc, char *argv[]) {
+void
+refine_mesh(Eigen::MatrixXd& V,
+           Eigen::MatrixXi& F,
+           Eigen::MatrixXd& uv,
+           Eigen::MatrixXi& FT,
+           int refinement)
+{
+    igl::upsample(V, F, refinement);
+    igl::upsample(uv, FT, refinement);
+}
+
+int
+main(int argc, char* argv[])
+{
   // Build maps from strings to enums
   std::map<std::string, spdlog::level::level_enum> log_level_map{
-      {"trace", spdlog::level::trace},       {"debug", spdlog::level::debug},
-      {"info", spdlog::level::info},         {"warn", spdlog::level::warn},
-      {"critical", spdlog::level::critical}, {"off", spdlog::level::off},
+    { "trace", spdlog::level::trace },       { "debug", spdlog::level::debug },
+    { "info", spdlog::level::info },         { "warn", spdlog::level::warn },
+    { "critical", spdlog::level::critical }, { "off", spdlog::level::off },
   };
 
   // Get command line arguments
-  CLI::App app{"Optimize Clough-Tocher cubic surface mesh."};
+  CLI::App app{ "Optimize Clough-Tocher cubic surface mesh." };
   std::string input_filename = "";
   std::string output_name = "./";
   spdlog::level::level_enum log_level = spdlog::level::warn;
@@ -29,17 +43,21 @@ int main(int argc, char *argv[]) {
   int iterations = 1;
   double scale = 1.;
   bool visualize = false;
+  int refinement = 0;
   app.add_option("-i,--input", input_filename, "Mesh filepath")
-      ->check(CLI::ExistingFile)
-      ->required();
+    ->check(CLI::ExistingFile)
+    ->required();
   app.add_option("--log_level", log_level, "Level of logging")
-      ->transform(CLI::CheckedTransformer(log_level_map, CLI::ignore_case));
-  app.add_option("-w,--weight", weight,
-                 "Fitting weight for the quadratic surface approximation")
-      ->check(CLI::PositiveNumber);
-  app.add_option("-n,--iterations", iterations,
-                 "Number of iterations of optimization");
+    ->transform(CLI::CheckedTransformer(log_level_map, CLI::ignore_case));
+  app
+    .add_option("-w,--weight",
+                weight,
+                "Fitting weight for the quadratic surface approximation")
+    ->check(CLI::PositiveNumber);
+  app.add_option(
+    "-n,--iterations", iterations, "Number of iterations of optimization");
   app.add_option("--scale", scale, "Scale input mesh");
+  app.add_option("--refinement", refinement, "Levels of refinement");
   app.add_option("-o, --output", output_name, "Output file prefix");
   app.add_flag("-v, --visualize", visualize, "Visualize with polyscope");
   CLI11_PARSE(app, argc, argv);
@@ -58,6 +76,8 @@ int main(int argc, char *argv[]) {
   spdlog::info("{} vertices", V.rows());
   if (scale != 1.)
     V *= scale;
+
+  refine_mesh(V, F, uv, FT, refinement);
 
   Eigen::VectorXd double_area;
   igl::doublearea(V, F, double_area);
@@ -82,20 +102,23 @@ int main(int argc, char *argv[]) {
   Eigen::SparseMatrix<double> fit_matrix;
   Eigen::SparseMatrix<double> energy_hessian;
   Eigen::CholmodSupernodalLLT<Eigen::SparseMatrix<double>>
-      energy_hessian_inverse;
-  CloughTocherSurface ct_surface(V, affine_manifold, optimization_params,
-                                 fit_matrix, energy_hessian,
+    energy_hessian_inverse;
+  CloughTocherSurface ct_surface(V,
+                                 affine_manifold,
+                                 optimization_params,
+                                 fit_matrix,
+                                 energy_hessian,
                                  energy_hessian_inverse);
   // WARNING: surface writing needed to generate points
   // TODO: make part of initialization
   ct_surface.write_cubic_surface_to_msh_with_conn_from_lagrange_nodes("initial",
                                                                       true);
   ct_surface.write_degenerate_cubic_surface_to_msh_with_conn(
-      "CT_degenerate_cubic_bezier_points", V, F);
+    "CT_degenerate_cubic_bezier_points", V, F);
   std::vector<Eigen::Vector3d> bezier_control_points =
-      generate_linear_clough_tocher_surface(ct_surface, V);
-  write_mesh(ct_surface, bezier_control_points,
-             join_path(output_name, "linear"));
+    generate_linear_clough_tocher_surface(ct_surface, V);
+  write_mesh(
+    ct_surface, bezier_control_points, join_path(output_name, "linear"));
 
   // initialize optimizer
   CloughTocherOptimizer optimizer(V, F, affine_manifold);
@@ -103,15 +126,17 @@ int main(int argc, char *argv[]) {
 
   // optimize the bezier nodes with laplacian energy
   std::vector<Eigen::Vector3d> laplacian_control_points =
-      optimizer.optimize_laplacian_energy(bezier_control_points);
-  write_mesh(ct_surface, laplacian_control_points,
+    optimizer.optimize_laplacian_energy(bezier_control_points);
+  write_mesh(ct_surface,
+             laplacian_control_points,
              join_path(output_name, "laplacian_mesh"));
 
   // optimize the bezier nodes with laplace beltrami energy
   std::vector<Eigen::Vector3d> laplace_beltrami_control_points =
-      optimizer.optimize_laplace_beltrami_energy(bezier_control_points,
-                                                 iterations);
-  write_mesh(ct_surface, laplace_beltrami_control_points,
+    optimizer.optimize_laplace_beltrami_energy(bezier_control_points,
+                                               iterations);
+  write_mesh(ct_surface,
+             laplace_beltrami_control_points,
              join_path(output_name, "laplace_beltrami_mesh"));
 
   set_bezier_control_points(ct_surface, laplace_beltrami_control_points);
@@ -123,19 +148,18 @@ int main(int argc, char *argv[]) {
   std::vector<SpatialVector> points;
   std::vector<std::vector<int>> polylines;
   ct_surface.discretize_patch_boundaries(3, points, polylines, true);
-  write_polylines_to_obj(join_path(output_name, "patch_boundaries.obj"), points,
-                         polylines);
+  write_polylines_to_obj(
+    join_path(output_name, "patch_boundaries.obj"), points, polylines);
   ct_surface.discretize_patch_boundaries(3, points, polylines, false);
   write_polylines_to_obj(
-      join_path(output_name, "interior_patch_boundaries.obj"), points,
-      polylines);
+    join_path(output_name, "interior_patch_boundaries.obj"), points, polylines);
 
   // write lag2bezier mat for c1meshing soft constraint
   Eigen::SparseMatrix<double, 1> l2b_mat;
   ct_surface.lag2bezier_full_mat(l2b_mat);
   Eigen::saveMarket(l2b_mat, "CT_lag2bezier_matrix.txt");
 
-  ct_surface.add_surface_to_viewer({1, 0.4, 0.3}, 3);
+  ct_surface.add_surface_to_viewer({ 1, 0.4, 0.3 }, 3);
   polyscope::registerSurfaceMesh("PL mesh", V, F);
   polyscope::screenshot(join_path(output_name, "render.png"));
   if (visualize) {
