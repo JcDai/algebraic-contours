fced128862d57b911153d2a026839a69eadf8bba
JcDai <jiachengdai97@gmail.com>
2025-09-24 15:52:24 -0400

    rearrange directory

--- src/clough_tocher_surface/clough_tocher_patch.cpp
+++ src/clough_tocher_surface/clough_tocher_patch.cpp
@@ -8,14 +8,15 @@
 #include <polyscope/surface_mesh.h>
 
 const std::array<Eigen::Matrix<double, 3, 3>, 3>
-    CloughTocherPatch::m_CTtri_bounds = CT_subtri_bound_matrices();
+  CloughTocherPatch::m_CTtri_bounds = CT_subtri_bound_matrices();
 
 const std::array<Eigen::Matrix<double, 10, 12>, 3>
-    CloughTocherPatch::m_CT_matrices = CT_subtri_matrices();
+  CloughTocherPatch::m_CT_matrices = CT_subtri_matrices();
 
 CloughTocherPatch::CloughTocherPatch(
-    Eigen::Matrix<double, 12, 3> &boundary_data)
-    : m_boundary_data(boundary_data) {
+  Eigen::Matrix<double, 12, 3>& boundary_data)
+  : m_boundary_data(boundary_data)
+{
 
   // TODO: old deprecated code, hij are quadratic, wrong , just here for
   // reference
@@ -40,47 +41,48 @@ CloughTocherPatch::CloughTocherPatch(
   }
 }
 
-void CloughTocherPatch::set_lagrange_nodes(
+void
+CloughTocherPatch::set_lagrange_nodes(
   const std::array<Eigen::Vector2d, 19>& planar_nodes,
   const std::array<Eigen::Vector3d, 19>& lagrange_nodes)
 {
   // TODO: Obtained from affine_manifold.cpp. Make standalone function
-  const std::array<PlanarPoint, 19> CT_nodes = {{
-      PlanarPoint(1., 0.),           // b0    0
-      PlanarPoint(0., 1.),           // b1    1
-      PlanarPoint(0., 0.),           // b2    2
-      PlanarPoint(2. / 3., 1. / 3.), // b01   3
-      PlanarPoint(1. / 3., 2. / 3.), // b10   4
-      PlanarPoint(0., 2. / 3.),      // b12   5
-      PlanarPoint(0., 1. / 3.),      // b21   6
-      PlanarPoint(1. / 3., 0.),      // b20   7
-      PlanarPoint(2. / 3., 0.),      // b02   8
-      PlanarPoint(4. / 9., 4. / 9.), // b01^c 9
-      PlanarPoint(1. / 9., 4. / 9.), // b12^c 10
-      PlanarPoint(4. / 9., 1. / 9.), // b20^c 11
-      PlanarPoint(7. / 9., 1. / 9.), // b0c   12
-      PlanarPoint(5. / 9., 2. / 9.), // bc0   13
-      PlanarPoint(1. / 9., 7. / 9.), // b1c   14
-      PlanarPoint(2. / 9., 5. / 9.), // bc1   15
-      PlanarPoint(1. / 9., 1. / 9.), // b2c   16
-      PlanarPoint(2. / 9., 2. / 9.), // bc2   17
-      PlanarPoint(1. / 3., 1. / 3.), // bc    18
-  }};
+  const std::array<PlanarPoint, 19> CT_nodes = { {
+    PlanarPoint(1., 0.),           // b0    0
+    PlanarPoint(0., 1.),           // b1    1
+    PlanarPoint(0., 0.),           // b2    2
+    PlanarPoint(2. / 3., 1. / 3.), // b01   3
+    PlanarPoint(1. / 3., 2. / 3.), // b10   4
+    PlanarPoint(0., 2. / 3.),      // b12   5
+    PlanarPoint(0., 1. / 3.),      // b21   6
+    PlanarPoint(1. / 3., 0.),      // b20   7
+    PlanarPoint(2. / 3., 0.),      // b02   8
+    PlanarPoint(4. / 9., 4. / 9.), // b01^c 9
+    PlanarPoint(1. / 9., 4. / 9.), // b12^c 10
+    PlanarPoint(4. / 9., 1. / 9.), // b20^c 11
+    PlanarPoint(7. / 9., 1. / 9.), // b0c   12
+    PlanarPoint(5. / 9., 2. / 9.), // bc0   13
+    PlanarPoint(1. / 9., 7. / 9.), // b1c   14
+    PlanarPoint(2. / 9., 5. / 9.), // bc1   15
+    PlanarPoint(1. / 9., 1. / 9.), // b2c   16
+    PlanarPoint(2. / 9., 2. / 9.), // bc2   17
+    PlanarPoint(1. / 3., 1. / 3.), // bc    18
+  } };
 
   // TODO: make global
-  std::array<std::array<int64_t, 10>, 3> local_nodes = {{{{0, 1, 18, 3, 4, 14, 15, 13, 12, 9}},
-    {{1, 2, 18, 5, 6, 16, 17, 15, 14, 10}},
-    {{2, 0, 18, 7, 8, 12, 13, 17, 16, 11}}}};
-  //std::array<int64_t, 10> perm = {0, 9, 3, 4, 7, 8, 6, 2, 1, 5};
+  std::array<std::array<int64_t, 10>, 3> local_nodes = {
+    { { { 0, 1, 18, 3, 4, 14, 15, 13, 12, 9 } },
+      { { 1, 2, 18, 5, 6, 16, 17, 15, 14, 10 } },
+      { { 2, 0, 18, 7, 8, 12, 13, 17, 16, 11 } } }
+  };
+  // std::array<int64_t, 10> perm = {0, 9, 3, 4, 7, 8, 6, 2, 1, 5};
 
   Eigen::Matrix<double, 10, 10> l2m;
   Eigen::Matrix<double, 10, 1> l;
 
-  for (int i = 0; i < 3; ++i)
-  {
+  for (int i = 0; i < 3; ++i) {
     for (int d = 0; d < 3; ++d) {
-      for (int j = 0; j < 10; ++j)
-      {
+      for (int j = 0; j < 10; ++j) {
         int n = local_nodes[i][j];
         double u = planar_nodes[n][0];
         double v = planar_nodes[n][1];
@@ -88,14 +90,14 @@ void CloughTocherPatch::set_lagrange_nodes(
         u = CT_nodes[n][0];
         v = CT_nodes[n][1];
         w = 1. - u - v;
-        //l2m.col(j) = monomial_basis_eval(u, v, w);
+        // l2m.col(j) = monomial_basis_eval(u, v, w);
         l2m.row(j) = monomial_basis_eval(u, v, w);
         l[j] = lagrange_nodes[n][d];
       }
-      //Eigen::SparseLU<Eigen::Matrix<double, 10, 10>> solver;
-      //solver.compute(l2m);
+      // Eigen::SparseLU<Eigen::Matrix<double, 10, 10>> solver;
+      // solver.compute(l2m);
       m_CT_coeffs[i].col(d) = l2m.fullPivLu().solve(l);
-      //m_CT_coeffs[i].col(d) = l2m * l;
+      // m_CT_coeffs[i].col(d) = l2m * l;
 
       /*
       for (int j = 0; j < 10; ++j)
@@ -110,19 +112,22 @@ void CloughTocherPatch::set_lagrange_nodes(
   }
 }
 
-int CloughTocherPatch::triangle_ind(const double &u, const double &v,
-                                    const double &w) const {
+int
+CloughTocherPatch::triangle_ind(const double& u,
+                                const double& v,
+                                const double& w) const
+{
   int idx = -1;
   for (int i = 0; i < 3; ++i) {
     if (m_CTtri_bounds[i](0, 0) * u + m_CTtri_bounds[i](0, 1) * v +
-                m_CTtri_bounds[i](0, 2) * w >=
-            -1e-7 &&
+            m_CTtri_bounds[i](0, 2) * w >=
+          -1e-7 &&
         m_CTtri_bounds[i](1, 0) * u + m_CTtri_bounds[i](1, 1) * v +
-                m_CTtri_bounds[i](1, 2) * w >=
-            -1e-7 &&
+            m_CTtri_bounds[i](1, 2) * w >=
+          -1e-7 &&
         m_CTtri_bounds[i](2, 0) * u + m_CTtri_bounds[i](2, 1) * v +
-                m_CTtri_bounds[i](2, 2) * w >=
-            -1e-7) {
+            m_CTtri_bounds[i](2, 2) * w >=
+          -1e-7) {
       idx = i;
       break;
     }
@@ -133,8 +138,10 @@ int CloughTocherPatch::triangle_ind(const double &u, const double &v,
 }
 
 Eigen::Matrix<double, 10, 1>
-CloughTocherPatch::monomial_basis_eval(const double &u, const double &v,
-                                       const double &w) const {
+CloughTocherPatch::monomial_basis_eval(const double& u,
+                                       const double& v,
+                                       const double& w) const
+{
   Eigen::Matrix<double, 10, 1> monomial_basis_values;
   monomial_basis_values(0, 0) = w * w * w; // w3
   monomial_basis_values(1, 0) = v * w * w; // vw2
@@ -150,9 +157,9 @@ CloughTocherPatch::monomial_basis_eval(const double &u, const double &v,
   return monomial_basis_values;
 }
 
-PlanarPoint normalize_domain_point(
-  const ConvexPolygon& domain,
-  const PlanarPoint& domain_point)
+PlanarPoint
+normalize_domain_point(const ConvexPolygon& domain,
+                       const PlanarPoint& domain_point)
 {
   // Get domain triangle vertices
   MatrixXr domain_vertices = domain.get_vertices();
@@ -172,12 +179,15 @@ PlanarPoint normalize_domain_point(
   return (domain_point - translation) * linear_transformation.inverse();
 }
 
-Eigen::Matrix<double, 3, 1> CloughTocherPatch::CT_eval_normalized(int idx, const double &u,
-                                                       const double &v) const {
+Eigen::Matrix<double, 3, 1>
+CloughTocherPatch::CT_eval_normalized(int idx,
+                                      const double& u,
+                                      const double& v) const
+{
   const double w = 1.0 - u - v;
   // std::cout << "subtri_idx: " << idx << std::endl;
   Eigen::Matrix<double, 10, 1> bb_vector =
-      CloughTocherPatch::monomial_basis_eval(u, v, w);
+    CloughTocherPatch::monomial_basis_eval(u, v, w);
 
   // std::cout << "monomial: " << bb_vector << std::endl;
 
@@ -186,14 +196,15 @@ Eigen::Matrix<double, 3, 1> CloughTocherPatch::CT_eval_normalized(int idx, const
   return val;
 }
 
-Eigen::Matrix<double, 3, 1> CloughTocherPatch::CT_eval(const double &u,
-                                                       const double &v) const {
+Eigen::Matrix<double, 3, 1>
+CloughTocherPatch::CT_eval(const double& u, const double& v) const
+{
   const double w = 1.0 - u - v;
   int idx = CloughTocherPatch::triangle_ind(u, v, w);
 
   // std::cout << "subtri_idx: " << idx << std::endl;
   Eigen::Matrix<double, 10, 1> bb_vector =
-      CloughTocherPatch::monomial_basis_eval(u, v, w);
+    CloughTocherPatch::monomial_basis_eval(u, v, w);
 
   // std::cout << "monomial: " << bb_vector << std::endl;
 
@@ -203,21 +214,25 @@ Eigen::Matrix<double, 3, 1> CloughTocherPatch::CT_eval(const double &u,
 }
 
 std::array<Eigen::Matrix<double, 10, 3>, 3>
-CloughTocherPatch::get_coeffs() const {
+CloughTocherPatch::get_coeffs() const
+{
   return m_CT_coeffs;
 }
 
-double CloughTocherPatch::external_boundary_data_eval(
-    const double &u, const double &v,
-    Eigen::Matrix<double, 12, 1> &external_boundary_data) const {
+double
+CloughTocherPatch::external_boundary_data_eval(
+  const double& u,
+  const double& v,
+  Eigen::Matrix<double, 12, 1>& external_boundary_data) const
+{
   const double w = 1.0 - u - v;
   int idx = CloughTocherPatch::triangle_ind(u, v, w);
 
   Eigen::Matrix<double, 10, 1> bb_vector =
-      CloughTocherPatch::monomial_basis_eval(u, v, w);
+    CloughTocherPatch::monomial_basis_eval(u, v, w);
 
   double value =
-      (m_CT_matrices[idx] * external_boundary_data).transpose() * bb_vector;
+    (m_CT_matrices[idx] * external_boundary_data).transpose() * bb_vector;
 
   return value;
 }
@@ -227,16 +242,16 @@ CloughTocherPatch::triangulate(size_t num_refinements,
                                std::array<Eigen::MatrixXd, 3>& V,
                                std::array<Eigen::MatrixXi, 3>& F) const
 {
-  for (int n = 0; n < 3; ++n)
-  {
+  for (int n = 0; n < 3; ++n) {
     // Triangulate the domain
     Eigen::MatrixXd V_domain;
     std::array<Eigen::Matrix<double, 3, 1>, 3> boundary_segments_coeffs;
-    for (int i = 0; i < 3; ++i)
-    {
+    for (int i = 0; i < 3; ++i) {
       boundary_segments_coeffs[i][0] = m_CTtri_bounds[n](i, 2);
-      boundary_segments_coeffs[i][1] = m_CTtri_bounds[n](i, 0) - m_CTtri_bounds[n](i, 2);
-      boundary_segments_coeffs[i][2] = m_CTtri_bounds[n](i, 1) - m_CTtri_bounds[n](i, 2);
+      boundary_segments_coeffs[i][1] =
+        m_CTtri_bounds[n](i, 0) - m_CTtri_bounds[n](i, 2);
+      boundary_segments_coeffs[i][2] =
+        m_CTtri_bounds[n](i, 1) - m_CTtri_bounds[n](i, 2);
     }
 
     ConvexPolygon domain(boundary_segments_coeffs);
@@ -247,7 +262,7 @@ CloughTocherPatch::triangulate(size_t num_refinements,
     for (Eigen::Index i = 0; i < V_domain.rows(); ++i) {
       SpatialVector surface_point;
       SpatialVector surface_normal;
-      //surface_point = CT_eval_normalized(V_domain(i, 0), V_domain(i, 1));
+      // surface_point = CT_eval_normalized(V_domain(i, 0), V_domain(i, 1));
       surface_point = CT_eval(V_domain(i, 0), V_domain(i, 1));
       V[n].row(i) = surface_point;
     }
@@ -255,12 +270,12 @@ CloughTocherPatch::triangulate(size_t num_refinements,
 }
 
 void
-CloughTocherPatch::triangulate_normalized(size_t num_refinements,
-                               std::array<Eigen::MatrixXd, 3>& V,
-                               std::array<Eigen::MatrixXi, 3>& F) const
+CloughTocherPatch::triangulate_normalized(
+  size_t num_refinements,
+  std::array<Eigen::MatrixXd, 3>& V,
+  std::array<Eigen::MatrixXi, 3>& F) const
 {
-  for (int n = 0; n < 3; ++n)
-  {
+  for (int n = 0; n < 3; ++n) {
     Eigen::MatrixXd V_domain;
     Eigen::Matrix<double, 3, 2> vertices;
     vertices << 0., 0., 0., 1., 1., 0.;
@@ -278,38 +293,41 @@ CloughTocherPatch::triangulate_normalized(size_t num_refinements,
   }
 }
 
-void CloughTocherPatch::parametrize_patch_boundaries(std::array<std::array<LineSegment, 3>, 3>& patch_boundaries) const
+void
+CloughTocherPatch::parametrize_patch_boundaries(
+  std::array<std::array<LineSegment, 3>, 3>& patch_boundaries) const
 {
-  for (int n = 0; n < 3; ++n)
-  {
+  for (int n = 0; n < 3; ++n) {
     // Triangulate the domain
     Eigen::MatrixXd V_domain;
     std::array<Eigen::Matrix<double, 3, 1>, 3> boundary_segments_coeffs;
-    for (int i = 0; i < 3; ++i)
-    {
+    for (int i = 0; i < 3; ++i) {
       boundary_segments_coeffs[i][0] = m_CTtri_bounds[n](i, 2);
-      boundary_segments_coeffs[i][1] = m_CTtri_bounds[n](i, 0) - m_CTtri_bounds[n](i, 2);
-      boundary_segments_coeffs[i][2] = m_CTtri_bounds[n](i, 1) - m_CTtri_bounds[n](i, 2);
+      boundary_segments_coeffs[i][1] =
+        m_CTtri_bounds[n](i, 0) - m_CTtri_bounds[n](i, 2);
+      boundary_segments_coeffs[i][2] =
+        m_CTtri_bounds[n](i, 1) - m_CTtri_bounds[n](i, 2);
     }
     ConvexPolygon domain(boundary_segments_coeffs);
     domain.parametrize_patch_boundaries(patch_boundaries[n]);
   }
 }
 
-void CloughTocherPatch::view() const
+void
+CloughTocherPatch::view() const
 {
- // Generate mesh discretization
+  // Generate mesh discretization
   std::array<Eigen::MatrixXd, 3> V;
   std::array<Eigen::MatrixXi, 3> F;
   triangulate(3, V, F);
 
   // Add surface mesh
   polyscope::init();
-  for (int i = 0; i < 3; ++i)
-  {
+  for (int i = 0; i < 3; ++i) {
     polyscope::registerSurfaceMesh("patch " + std::to_string(i), V[i], F[i])
       ->setEdgeWidth(0);
-    polyscope::registerPointCloud("control points" + std::to_string(i), m_CT_coeffs[i]);
+    polyscope::registerPointCloud("control points" + std::to_string(i),
+                                  m_CT_coeffs[i]);
   }
 
   polyscope::show();
